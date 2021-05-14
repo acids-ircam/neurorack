2cd8870963eaf3535185f3d1c458a69358acd214
Philippe Esling <esling@PRO-2903-esling.local>
2021-05-14 18:49:21 +0200

    Updating code

--- code/stats.py
+++ code/stats.py
@@ -1,18 +1,42 @@
+"""
+
+ ~ Neurorack project ~
+ Stats : Retrieve a set of system properties
+ 
+ This allows to check the system state (to be used on the LCD display).
+ Currently, the stats retrieved are :
+     - IP of the board
+     - CPU Load
+     - Memory use
+     - Disk Use
+     - CPU Temperature
+ 
+ Author               : Philippe Esling, Ninon Devis, Martin Vert
+                        <{esling, devis}@ircam.fr>
+
+"""
+
 import time
 import subprocess
 
 class Stats():
-
-    """
-    Initialize stats function
-    """
-    def __init__(self, background=True):
+    '''
+        The Stats class contains system properties and allows to retrieve them
+    '''
+    def __init__(self):
+        '''
+            Constructor - Creates new instance and retrieve initial stats
+        '''
         # Retrieve stats
         self.retrieve_stats()
 
     def retrieve_stats(self):
-        # Shell scripts for system monitoring from here:
-        # https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
+        '''
+            Constructor - Creates a new instance of the Navigation class.
+            Shell scripts for system monitoring from here:
+            https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
+            @TODO: Would be useful to monitor GPU states and also audio engine 
+        '''
         cmd = "hostname -I | cut -d\' \' -f1"
         self.ip = subprocess.check_output(cmd, shell=True).decode("utf-8")
         cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
