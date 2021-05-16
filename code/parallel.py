356768f245d41e834f1e1d221fab07d0f59e5487
Philippe Esling <esling@PRO-2903-esling.local>
2021-05-16 15:10:12 +0200

    Update doc of parallel

--- code/parallel.py
+++ code/parallel.py
@@ -1,33 +1,110 @@
+"""
+
+ ~ Neurorack project ~
+ Parallel : Base classes for handling parallel system
+ 
+ This file defines all multiprocessing and multi-threading classes.
+ 
+ Author               : Philippe Esling, Ninon Devis, Martin Vert
+                        <{esling, devis}@ircam.fr>
+
+"""
+
 import multiprocessing as mp
 from multiprocessing import Process, Queue
 
 class Input:
+    '''
+        The Input class defines basic functions for neurorack input.
+    '''
     
-    def __init__(self, name):
+    def __init__(self, 
+                 name: str):
+        '''
+            Constructor - Creates a new instance of the Input class.
+            Parameters:
+                name:       [str]
+                            Name of the input
+        '''
         print('Initializing input ' + name)
         self.name = name
 
     def callback(self, state, queue):
+        '''
+            Function for starting the input.
+            Here it will be a sequential (blocking) input
+            Parameters:
+                state:      [Manager]
+                            Shared memory through a Multiprocessing manager
+                queue:      [Queue]
+                            Shared memory queue through a Multiprocessing queue
+        '''
         print(self.name)
         
 class ThreadInput(Input):
+    '''
+        The ThreadInput class defines threaded versions of the inputs.
+    '''
 
-    def __init__(self):
-        super().__init__(name)
-
-class ProcessInput(Input):
-
-    def __init__(self, name):
+    def __init__(self, 
+                 name: str):
+        '''
+            Constructor - Creates a new instance of the ThreadInput class.
+            Parameters:
+                name:       [str]
+                            Name of the input
+        '''
         super().__init__(name)
 
     def callback(self, state, queue):
+        '''
+            Function for starting the input.
+            Here it will be a threaded input
+            Parameters:
+                state:      [Manager]
+                            Shared memory through a Multiprocessing manager
+                queue:      [Queue]
+                            Shared memory queue through a Multiprocessing queue
+        '''
+        super().callback(state, queue)
+        print(mp.current_process())
+
+class ProcessInput(Input):
+
+    def __init__(self, 
+                 name: str):
+        '''
+            Constructor - Creates a new instance of the ProcessInput class.
+            Parameters:
+                name:       [str]
+                            Name of the input
+        '''
+        super().__init__(name)
+
+    def callback(self, state, queue):
+        '''
+            Function for starting the input.
+            Here it will be an independent process input
+            Parameters:
+                state:      [Manager]
+                            Shared memory through a Multiprocessing manager
+                queue:      [Queue]
+                            Shared memory queue through a Multiprocessing queue
+        '''
         super().callback(state, queue)
         print(mp.current_process())
 
 class InterruptInput(Input):
 
-    def __init__(self):
-        print('Init')
+    def __init__(self, 
+                 name: str):
+        '''
+            Constructor - Creates a new instance of the InterruptInput class.
+            Parameters:
+                name:       [str]
+                            Name of the input
+        '''
+        super().__init__(name)
 
 if __name__ == '__main__':
     print('Testing parallel system')
