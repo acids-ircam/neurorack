0a38dcc96bb71c52c4a4f4ac2a0df60452fa3d3d
Philippe Esling <esling@PRO-2903-esling.local>
2021-05-16 22:07:57 +0200

    Creating tutorial

--- code/main.py
+++ code/main.py
@@ -14,20 +14,22 @@ import Jetson.GPIO as GPIO
 from rotary import Rotary
 from cv import CVChannels
 from audio import Audio
-#from button import Button
+from button import Button
 import multiprocessing as mp
 from multiprocessing import Process, Manager, Queue
 
 class Neurorack():
     '''
-        The Neurorack main class
+        The Neurorack main class is responsible for starting all processes.
+            - Audio engine
+            - Rotary encoder
+            - CV Channels
+            - Screen
     '''
-    
-    N_CVs = 6
 
     def __init__(self):
         '''
-            Constructor - Creates a new instance of the Navigation class.
+            Constructor - Creates a new instance of the Neurorack class.
         '''
         # Init states of information
         self.init_state()
@@ -37,12 +39,15 @@ class Neurorack():
         self.rotary = Rotary()
         # Create CV channels
         self.cvs = CVChannels()
+        # Create push button
+        self.button = Button()
+        # Perform GPIO cleanup
         GPIO.cleanup()
         # Need to import Screen after cleanup
         from screen import Screen
         self.screen = Screen()
         # List of objects to create processes
-        self.objects = [self.screen, self.rotary, self.cvs]
+        self.objects = [self.audio, self.screen, self.rotary, self.cvs, self.button]
         # Find number of CPUs
         self.nb_cpus = mp.cpu_count()
         # Create a pool of jobs
@@ -55,16 +60,24 @@ class Neurorack():
 
     def init_state(self):
         '''
-            Initialize the shared memory state for the full rack
+            Initialize the shared memory state for the full rack.
+            The global properties are shared by a multiprocessing manager.
         '''
         # Use a multi-processing Manager
         self.manager = Manager()
         self.state = self.manager.dict()
+        self.state['global'] = self.manager.dict()
         self.state['cv'] = self.manager.list([0.0] * self.N_CVs)
         self.state['rotary'] = 0
         self.state['button'] = 0
         self.state['menu'] = 0
         self.state['audio'] = 0
+        
+    def set_signals(self):
+        pass
+    
+    def set_callbacks(self):
+        pass
             
     def start(self):
         '''
@@ -90,7 +103,7 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description='Neurorack')
     # Device Information
-    parser.add_argument('--device',         type=str, default='cuda:2',     help='device cuda or cpu')
+    parser.add_argument('--device',         type=str, default='cuda:0',     help='device cuda or cpu')
     # Parse the arguments
     args = parser.parse_args()
     neuro = Neurorack()
