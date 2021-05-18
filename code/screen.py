06a87638e99dadcba555540b7cba32cf1e5b477d
Philippe Esling <esling@ircam.fr>
2021-05-18 14:36:08 +0200

    Adding global configuration classes

--- code/screen.py
+++ code/screen.py
@@ -21,7 +21,7 @@ from parallel import ProcessInput
 from graphics.utils import get_resized_image
 from stats import Stats
 from multiprocessing import Event
-import config as config
+import .config as config
 
 class Screen(ProcessInput):
     '''
