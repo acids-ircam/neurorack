0f514da6e21b9241f24b422bd3ae1123bb361cbe
Philippe Esling <esling@ircam.fr>
2021-05-19 11:31:05 +0200

    Advancing menu

--- code/graphics/config.py
+++ code/graphics/config.py
@@ -40,3 +40,17 @@ class config:
         none        = -1
         button      = 0
         rotary      = 1
+    
+    # Global screen info
+    class screen:
+        # Screen modes
+        mode_init   = 0
+        mode_main   = 1
+        mode_menu   = 2
+        # General screen properties
+        height      = 240
+        width       = 180
+        # Display config
+        padding     = -2
+        main_x      = 0
+        bg_image    = './data/acids.png'
