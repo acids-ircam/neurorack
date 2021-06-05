befbd71c11831ddb51c58d01f83fab65f856e59e
ninon-io <ninon.devis@ircam.fr>
2021-06-05 18:35:48 +0200

    play works?

--- code/models/nsf_impacts.py
+++ code/models/nsf_impacts.py
@@ -38,10 +38,10 @@ class NSF:
         return features
 
     def preload(self):
-        self._model = torch.load(self.m_path, map_location="cpu")
+        self._model = torch.load(self.m_path, map_location="cuda")
         self._model = self._model.cuda()
         features = self.dummy_features()
-        features = torch.tensor(features).unsqueeze(0).cuda()
+        features = torch.tensor(features).unsqueeze(0).cuda().float()
         for p in range(self.f_pass):
             with torch.no_grad():
                 self._model(features)
